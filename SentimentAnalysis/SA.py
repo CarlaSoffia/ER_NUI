62be80baf2fdbf9a77b92757378792c7360bbbd8
carla <carlasofiamendes@outlook.com>
2023-03-17 11:58:22 +0000

    Added SA model & training file

--- SentimentAnalysis/SA.py
+++ SentimentAnalysis/SA.py
@@ -19,6 +19,7 @@ test_size = 0.03
 patience = 10
 epochs = 100
 batch = 64
+model_name = "models/model_"+str(version)+"_"+str(batch)+"b_"+str(epochs)+"e_"+str(seed)+"s.h5"
 labels = ["anger","disgust","fear","guilt","joy","sadness","shame"]
 if train:
     dataset = pd.read_csv("ISEAR_portuguese.csv",sep=";")
@@ -28,9 +29,7 @@ if train:
 
     # Convert labels to numerical values
     le = LabelEncoder()
-    print(dataset['sentiment'])
     dataset['sentiment'] = le.fit_transform(dataset['sentiment'])
-    print(dataset['sentiment'])
     # Split the dataset into training and testing sets
     train_texts, test_texts, train_labels, test_labels = train_test_split(dataset['text'], dataset['sentiment'], test_size=test_size, random_state=seed)
 
@@ -40,7 +39,7 @@ if train:
     train_sequences = tokenizer.texts_to_sequences(train_texts)
     test_sequences = tokenizer.texts_to_sequences(test_texts)
 
-    with open('tokenizer.pickle', 'wb') as handle:
+    with open('models/tokenizer.pickle', 'wb') as handle:
         pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     # Pad the sequences to have the same length
@@ -57,7 +56,7 @@ if train:
     ])
     early_stop = EarlyStopping(monitor='val_loss', patience=patience)
     version = version+1
-    checkpoint = ModelCheckpoint("models/model_"+str(version)+".h5", monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+    checkpoint = ModelCheckpoint(model_name=model_name, monitor='val_loss', save_best_only=True, mode='min', verbose=1)
     # Compile the model
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
@@ -69,15 +68,15 @@ if train:
     print(f'Test loss: {loss:.3f}')
     print(f'Test accuracy: {accuracy:.3f}')
 else:
-    model = keras.models.load_model("models/model_"+str(version)+".h5")    
-    with open('tokenizer.pickle', 'rb') as handle:
+    model = keras.models.load_model(model_name)    
+    with open('models/tokenizer.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
-    text_sequence = tokenizer.texts_to_sequences(["Hoje estive a passear e a ver o sol"])
+    text_sequence = tokenizer.texts_to_sequences(["Hoje estive a passear pela cidade e a ver o sol"])
     text_padded = pad_sequences(text_sequence, maxlen=num_words, padding='post', truncating='post')
 
     predictions = model.predict(text_padded)[0]
     maxVal = predictions.argmax()
-    print(maxVal)
     accuracy = predictions[maxVal]
     emotion = labels[maxVal]
+    print(predictions, labels)
     print(emotion, accuracy)
