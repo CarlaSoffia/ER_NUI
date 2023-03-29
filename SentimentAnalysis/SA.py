c98bdb2397988d80a26b9cfb1130d63b3a439aa1
carla <carlasofiamendes@outlook.com>
2023-03-29 12:07:46 +0100

    Update SA.py

--- SentimentAnalysis/SA.py
+++ SentimentAnalysis/SA.py
@@ -10,28 +10,60 @@ from tensorflow.keras.callbacks import EarlyStopping
 from keras.callbacks import ModelCheckpoint
 import glob
 import pickle
+import matplotlib.pyplot as plt
 
-train = False
+def saveModelArchitecture(file):
+    start_line = 33
+    end_line = 95
+
+    # Open the source file for reading
+    with open(__file__, 'r') as f:
+        # Read all the lines of the source file
+        lines = f.readlines()
+
+    # Extract the desired lines
+    new_lines = lines[start_line-1:end_line]
+
+    # Open a new file for writing
+    with open(file, 'w') as f:
+        # Write the extracted lines to the new file
+        for line in new_lines:
+            f.write(line)
+
+train = True
 version = len(glob.glob(f"models/*.h5"))
-num_words = 10000
-seed = 2
-test_size = 0.03
-patience = 10
+num_words = 5000
+np_seed = 0
+ts_seed = 42
+test_size = 0.3
+patience = 5
 epochs = 100
-batch = 64
-model_name = "models/model_"+str(version)+"_"+str(batch)+"b_"+str(epochs)+"e_"+str(seed)+"s.h5"
+batch = 128
+dropout = 0.8
+filter_units = 32
+data = "pt"
+data_augmentation = False
+if data_augmentation:
+    typeData = "data_augmentation"
+    path = "./"
+else:
+    typeData = "final"
+    path = "./datasets/preprocessed"
+file = path + "/ISEAR_only_text_"+data+"_"+typeData+".csv"
 labels = ["anger","disgust","fear","guilt","joy","sadness","shame"]
 if train:
-    dataset = pd.read_csv("ISEAR_portuguese.csv",sep=";")
-
-    np.random.seed(seed)
-    tf.random.set_seed(seed)
+    dataset = pd.read_csv(file,sep=";")
+    version = version + 1
+    model_name = "models/"+data+"_model_"+str(version)+"_"+str(batch)+"b_"+str(epochs)+"e_"+str(np_seed)+"nps_"+str(ts_seed)+"ts_"+str(dropout)[2:]+"d_"+str(test_size)[2:]+"ts_"+str(num_words)+"nw_"+typeData+"td.h5"
+    saveModelArchitecture("architectures/model_"+str(version)+"_architecture.py")
+    np.random.seed(np_seed)
+    tf.random.set_seed(ts_seed)
 
     # Convert labels to numerical values
     le = LabelEncoder()
     dataset['sentiment'] = le.fit_transform(dataset['sentiment'])
     # Split the dataset into training and testing sets
-    train_texts, test_texts, train_labels, test_labels = train_test_split(dataset['text'], dataset['sentiment'], test_size=test_size, random_state=seed)
+    train_texts, test_texts, train_labels, test_labels = train_test_split(dataset['text'], dataset['sentiment'], test_size=test_size, random_state=np_seed)
 
     # Tokenize the texts and convert them into sequences
     tokenizer = Tokenizer(num_words=num_words, oov_token='<OOV>')
@@ -48,26 +80,50 @@ if train:
 
     # Define the model
     model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(input_dim=num_words, output_dim=100, input_length=num_words),
-        tf.keras.layers.Conv1D(filters=64, kernel_size=5, activation='relu'),
-        tf.keras.layers.GlobalMaxPooling1D(),
-        tf.keras.layers.Dense(units=64, activation='relu'),
-        tf.keras.layers.Dense(units=7, activation='softmax')
+      tf.keras.layers.Embedding(input_dim=num_words, output_dim=100, input_length=num_words),
+      tf.keras.layers.Conv1D(filters=filter_units, kernel_size=3, activation='relu'),
+      tf.keras.layers.Dropout(dropout),
+      tf.keras.layers.Flatten(),
+      #tf.keras.layers.Dense(units=filter_units, activation='relu'),
+      #tf.keras.layers.Dropout(dropout),
+      tf.keras.layers.Dense(units=7, activation='softmax')
     ])
     early_stop = EarlyStopping(monitor='val_loss', patience=patience)
-    version = version+1
-    checkpoint = ModelCheckpoint(model_name=model_name, monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+    checkpoint = ModelCheckpoint(filepath=model_name, monitor='val_loss', save_best_only=True, mode='min', verbose=1)
     # Compile the model
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     # Train the model
-    model.fit(train_padded, train_labels, epochs=epochs, batch_size=batch,validation_data=(test_padded, test_labels), callbacks=[early_stop, checkpoint])
+    history = model.fit(train_padded, train_labels, epochs=epochs, batch_size=batch,validation_data=(test_padded, test_labels), callbacks=[early_stop, checkpoint])
 
     # Evaluate the model
     loss, accuracy = model.evaluate(test_padded, test_labels)
     print(f'Test loss: {loss:.3f}')
     print(f'Test accuracy: {accuracy:.3f}')
+
+    AccPNG = "graphics/accuracy_model_"+str(version)+"_"+str(batch)+"b_"+str(epochs)+"e_"+str(np_seed)+"nps_"+str(ts_seed)+"ts_"+"s_"+str(dropout)[2:]+"d_"+str(test_size)[2:]+"ts_"+str(num_words)+"_"+data+"_"+typeData+".png"
+    LossPNG = "graphics/loss_model_"+str(version)+"_"+str(batch)+"b_"+str(epochs)+"e_"+str(np_seed)+"nps_"+str(ts_seed)+"ts_"+"s_"+str(dropout)[2:]+"d_"+str(test_size)[2:]+"ts_"+str(num_words)+"_"+data+"_"+typeData+".png"
+    # Plot training and validation accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.ylabel(f'Accuracy - test accuracy: {accuracy:.3f}')
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    #plt.show()
+    plt.savefig(AccPNG)
+    # Plot training and validation loss
+    fig, ax = plt.subplots()
+    ax.plot(history.history['loss'], label='Training Loss')
+    ax.plot(history.history['val_loss'], label='Validation Loss')
+    ax.set_title('Training and Validation Loss')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel(f'Loss - test loss: {loss:.3f}')
+    ax.legend()
+    #plt.show()
+    plt.savefig(LossPNG)
 else:
+    model_name = "models/_"+data+"_model_"+str(version)+"_"+str(batch)+"b_"+str(epochs)+"e_"+str(seed)+"s.h5"    
     model = keras.models.load_model(model_name)    
     with open('models/tokenizer.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
